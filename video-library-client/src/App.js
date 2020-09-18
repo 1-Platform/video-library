3c1deb986a3cc7c1ae3e909e1754edd91b844077
Ghanshyam Lohar <gslohar@gmail.com>
2020-09-19 01:39:12 +0530

    Adding header footer to Video Library SPA

---
+++ video-library-client/src/App.js
@@ -0,0 +1,48 @@
+import React, { createRef, useEffect } from "react";
+import VideoLibrary from "./VideoLibrary";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import AddModal from "./video/AddModal";
+import EditModal from "./video/EditModal";
+import WatchModal from "./video/WatchModal";
+import "@patternfly/patternfly/patternfly.css";
+import "@one-platform/opc-header/dist/opc-header";
+import "@one-platform/opc-footer/dist/opc-footer";
+import "./VideoLibrary.css";
+
+const App = () => {
+  const breadcrumbs = createRef();
+  useEffect(() => {
+    breadcrumbs.current.opcHeaderBreadcrumb = [
+      {
+        name: "Home",
+        href: "/",
+      },
+      {
+        name: "Video Library",
+        href: "/video-library",
+      },
+    ];
+  }, []);
+  return (
+    <React.StrictMode>
+      <opc-header heading="Video Library" theme="blue">
+        <opc-header-breadcrumb
+          ref={breadcrumbs}
+          slot="breadcrumb"
+        ></opc-header-breadcrumb>
+      </opc-header>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route path="/" component={VideoLibrary} />
+        </Switch>
+        <Route path="/add" component={AddModal} />
+        <Route path="/watch/:id" component={WatchModal} />
+        <Route path="/edit/:id" component={EditModal} />
+      </BrowserRouter>
+      <opc-footer theme="blue">
+        <span slot="copyright">Copyright © 2020 All Rights Reserved.</span>
+      </opc-footer>
+    </React.StrictMode>
+  );
+};
+export default App;
